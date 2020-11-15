7e4c8768badc4e3260f507713c1c39916397d6f5
rysamuel9 <ronysamuel19@gmail.com>
2020-11-16 02:54:26 +0700

    cuti

---
+++ admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+        Copyright &copy; 2020 <a href="https://www.instagram.com/scola.id/?hl=id" target="_blank"> Scola LMS </a>
+        <span class="float-right"><a href="https://www.instagram.com/scola.id/?hl=id" target="_blank"> Powered By Scola LMS </a>
+        </span>
+    </footer>
