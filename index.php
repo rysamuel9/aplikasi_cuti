3fd895e4507e97cd65a51338e3eca7e00ad3da9f
rysamuel9 <ronysamuel19@gmail.com>
2020-11-16 03:32:28 +0700

    Baru

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- <link rel="shortcut icon" href="assets/ico/favicon.png"> -->
 
-    <title> Karyawan Scola LMS</title>
+    <title> Karyawan di Scola LMS</title>
 
     <!-- Icons -->
     <link href="css/font-awesome.min.css" rel="stylesheet">
